360b1b137b23925c8f4c99049cf02a34ce44c873
dannysalazarm07 <dannysalazarm07@gmail.com>
2021-11-17 20:30:49 -0500

    account ms finished

---
+++ account_ms/src/main/java/com/misiontic/account_ms/repositories/TransactionRepository.java
@@ -0,0 +1,13 @@
+package com.misiontic.account_ms.repositories;
+
+import com.misiontic.account_ms.models.Transaction;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface TransactionRepository  extends MongoRepository<Transaction, String> {
+    List<Transaction> findByUsernameOrigin(String usernameOrigin);
+    List<Transaction> findByUsernameDestiny(String usernameDestiny);
+
+}
